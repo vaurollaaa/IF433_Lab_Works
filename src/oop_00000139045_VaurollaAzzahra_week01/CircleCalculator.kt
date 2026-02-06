8dfa533791684b53c4521a994aadfeeee7d47886
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-06 16:40:55 +0700

    week01: refactor to use val and string tedmplates

--- src/oop_00000139045_VaurollaAzzahra_week01/CircleCalculator.kt
+++ src/oop_00000139045_VaurollaAzzahra_week01/CircleCalculator.kt
@@ -5,14 +5,14 @@ package oop_001.johnthor.week01
 fun main(args: Array<String>) {
 
     // Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius = 7.0
+    val pi = 3.14
 
     // Calculation
-    var area: Double = pi * radius * radius
+    val area = pi * radius * radius
 
     // Output Concatenation
-    println("Radius: " + radius + ", Area: " + area)
+    println("Radius: $radius, Area: $area")
 
     // Logic check
     checkSize(area)
