1d4d0af15cde854bf0425e41f262833fef5cd860
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-06 16:05:35 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000139045_VaurollaAzzahra_week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000139045_VaurollaAzzahra_week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
