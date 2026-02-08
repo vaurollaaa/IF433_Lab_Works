5d8c28a6eb3e8dc25eaccdf8df6499b1c767891d
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-08 22:27:30 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000139045_VaurollaAzzahra_week01/GameStore.kt
+++ src/oop_00000139045_VaurollaAzzahra_week01/GameStore.kt
@@ -1,8 +1,14 @@
 package oop_00000139045_VaurollaAzzahra_week01
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price * 80 / 100
+    else price * 90 / 100
+
 fun main () {
 
     val gameTitle: String = "Roblox"
     val price: Int = 100
+
+    val finalPrice = calculateDiscount(price)
 
 }
