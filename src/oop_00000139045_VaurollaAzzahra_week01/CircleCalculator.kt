602205b6bfe7eec23f77c8147dd8a343e93a6eb8
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-08 21:26:23 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000139045_VaurollaAzzahra_week01/CircleCalculator.kt
+++ src/oop_00000139045_VaurollaAzzahra_week01/CircleCalculator.kt
@@ -1,18 +1,13 @@
 package oop_001.johnthor.week01
 
-fun main(args: Array<String>) {
+fun main() {
 
-    // Variable definition
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     val area = pi * radius * radius
 
-    // Output Concatenation
     println("Radius: $radius, Area: $area")
-
-    // Logic check
     println(checkSize(area))
 }
 
