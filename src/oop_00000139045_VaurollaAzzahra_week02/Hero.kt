d006023347dc634f0fd13f8b63efd37c44685e15
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-13 23:14:29 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000139045_VaurollaAzzahra_week02/Hero.kt
@@ -0,0 +1,21 @@
+package oop_00000139045_VaurollaAzzahra_week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) hp = 0
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
