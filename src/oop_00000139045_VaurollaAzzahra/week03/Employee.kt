df0d531caa499d76118df71159caddaddbe2a891
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-20 15:39:11 +0700

    week03: add private property

--- src/oop_00000139045_VaurollaAzzahra/week03/Employee.kt
+++ src/oop_00000139045_VaurollaAzzahra/week03/Employee.kt
@@ -10,4 +10,18 @@ class Employee(val name: String) {
                 field = value // Gunakan field untuk assign nilai asli
             }
     }
+
+// Hanya bisa diakses di file/class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar rahasia
+    // kecuali kita buat function khusus untuk print.
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
