e0b93274a59c6ea6fead9f88b3cba31313ac9eb9
vaurollaaa <vaurollaazhr@gmail.com>
2026-02-20 15:23:53 +0700

    week03: simulate stack overflow error in setter

---
+++ src/oop_00000139045_VaurollaAzzahra/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000139045_VaurollaAzzahra.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Ini akan meledakkan program Anda
+}
