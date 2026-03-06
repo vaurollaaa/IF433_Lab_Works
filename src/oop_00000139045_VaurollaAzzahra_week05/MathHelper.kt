2844ebaf0928b3abf8402c4a5c27d88e4ab83914
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-06 14:49:42 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000139045_VaurollaAzzahra_week05/MathHelper.kt
@@ -0,0 +1,8 @@
+package oop_00000139045_VaurollaAzzahra_week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int) = sisi * sisi
+    fun hitungLuas(panjang: Int, lebar: Int) = panjang * lebar
+    fun hitungLuas(jariJari: Double) = 3.14 * jariJari * jariJari
+}
