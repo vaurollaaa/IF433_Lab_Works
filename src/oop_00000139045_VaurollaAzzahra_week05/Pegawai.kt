00d15648fe53171ea848a08e0bb29b76cc7ed705
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-06 11:47:51 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000139045_VaurollaAzzahra_week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000139045_VaurollaAzzahra_week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
