6847287c2dcf424f2ea58979a6290f549deadffa
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:53:36 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000139045_VaurollaAzzahra_week06/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week06/Main.kt
@@ -23,4 +23,12 @@ fun main() {
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
