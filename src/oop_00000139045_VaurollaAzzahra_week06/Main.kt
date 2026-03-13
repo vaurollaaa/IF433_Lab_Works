c854d7c086448ef8d607b71d839cbc6cfc7e64b5
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:51:19 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000139045_VaurollaAzzahra_week06/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week06/Main.kt
@@ -19,4 +19,8 @@ fun main() {
     println("\n=== TESTING CHECKOUT ===")
     processCheckout (method = pay1, amount = 50000.0)
     processCheckout (method = pay2, amount = 150000.0)
+
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
 }
