4aa24ed8d3081779ab9992c9e5d89796fc7c9a96
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:45:31 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000139045_VaurollaAzzahra_week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000139045_VaurollaAzzahra_week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnoff()
+            }
+        }
+    }
+}
