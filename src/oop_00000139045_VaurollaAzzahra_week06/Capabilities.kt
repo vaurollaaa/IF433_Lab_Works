73137faaaed57eb3ea3873445b50cbaa58a41f43
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:10:44 +0700

    week06: create compound interfaces

---
+++ src/oop_00000139045_VaurollaAzzahra_week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000139045_VaurollaAzzahra_week06
+
+// Interface Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
