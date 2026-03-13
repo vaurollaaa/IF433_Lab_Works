90b4735b3175e83fc020fa0b6a260a2932f46c6f
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:15:36 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000139045_VaurollaAzzahra_week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000139045_VaurollaAzzahra_week06
+
+interface Camera {
+    fun turnon() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnon() { println("Sinyal seluler mencari jaringan.") }
+}
