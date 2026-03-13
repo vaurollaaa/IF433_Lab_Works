eb25b1476ecebcb4d5fe70947ccf4931169fd83f
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:18:57 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000139045_VaurollaAzzahra_week06/Smartphone.kt
+++ src/oop_00000139045_VaurollaAzzahra_week06/Smartphone.kt
@@ -2,4 +2,10 @@ package oop_00000139045_VaurollaAzzahra_week06
 
 // ERROR: Class Smartphone inherits multiple implementations of turnon()
 class Smartphone: Camera, Phone {
+    // Manually override to resolve ambiguity
+    override fun turnon() {
+        super<Camera>.turnon() // Menjalankan logika Camera
+        super<Phone>.turnon() // Menjalankan logika Phone
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
