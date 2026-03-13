df029801144f3d6c789dbd97cf421809cc324740
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:03:49 +0700

    week06: create Button class implementing Clickable

--- src/oop_00000139045_VaurollaAzzahra_week06/Clickable.kt
+++ src/oop_00000139045_VaurollaAzzahra_week06/Clickable.kt
@@ -3,4 +3,10 @@ package oop_00000139045_VaurollaAzzahra_week06
 interface Clickable {
     val name: String // Abstract property, tidak ada nilai default
     fun click()
+
+    class Button(override val name: String) : Clickable {
+        override fun click() {
+            println("Tombol '$name' berhasil diklik!")
+        }
+    }
 }
