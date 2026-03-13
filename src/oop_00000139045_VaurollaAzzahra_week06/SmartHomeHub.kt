c6f1b41f787070a1c312a38fcf50b765e3a86d44
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-13 15:47:24 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000139045_VaurollaAzzahra_week06/SmartHomeHub.kt
+++ src/oop_00000139045_VaurollaAzzahra_week06/SmartHomeHub.kt
@@ -14,4 +14,15 @@ class SmartHomeHub {
             }
         }
     }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
 }
