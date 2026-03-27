06ef95f56f8817fe17e9e0472f18e74181309936
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-27 21:29:41 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000139045_VaurollaAzzahra_week07/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week07/Main.kt
@@ -20,4 +20,10 @@ fun main() {
     val data2 = DataUser("Alice", 22)
     println(data1) // Otomatis readable format
     println("Sama? ${data1 == data2}") // True (Structural Equality)
+
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    val (userName, userAge) = data1 // Destructuring Declaration
+    println("Destructured: $userName berumur $userAge")
 }
