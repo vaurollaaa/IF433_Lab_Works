4a00a3a8bf7a8e7b9fcc9321bba9ae9078ae7ecb
vaurollaaa <vaurollaazhr@gmail.com>
2026-03-27 21:26:36 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_00000139045_VaurollaAzzahra_week07/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week07/Main.kt
@@ -8,4 +8,10 @@ fun main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient() // Instansiasi lewat Factory
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1) // Akan mencetak memori hash
+    println("Sama? ${reg1 == reg2}") // False
 }
---
+++ src/oop_00000139045_VaurollaAzzahra_week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_00000139045_VaurollaAzzahra_week07
+
+class RegularUser (val name: String, val age: Int)
