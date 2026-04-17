45cc118bdc6fcd95f67af8e6dd0bfd3763c3c471
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 21:15:46 +0700

    week08: (task) process mixed raw JSON list using as?

---
+++ src/oop_00000139045_VaurollaAzzahra_week08/ECommerceMain.kt
@@ -0,0 +1,26 @@
+package oop_00000139045_VaurollaAzzahra_week08
+
+fun main() {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Corrupted warranty
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+
+    val parser = ApiParser()
+
+    println("=== STARTING BATCH COLLECTION ===")
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+            product?.let {
+                println("Parsed Product: $it")
+                parser.checkout(it)
+            }
+        } catch (e: IllegalArgumentException) {
+            println("Peringatan Data Korup: ${e.message}")
+        }
+    }
+}
