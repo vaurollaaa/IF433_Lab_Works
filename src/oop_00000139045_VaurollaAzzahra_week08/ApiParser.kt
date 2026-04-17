b8dc087d1d6ebefce57b448446dc66b87d19b6ab
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 21:10:26 +0700

    week08: (task) implement robust ApiParser using Elvis and

---
+++ src/oop_00000139045_VaurollaAzzahra_week08/ApiParser.kt
@@ -0,0 +1,22 @@
+package oop_00000139045_VaurollaAzzahra_week08
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id = requireNotNull(rawJson["id"] as? String) { "API Invalid: Missing ID" }
+        val name = requireNotNull(rawJson["name"] as? String) { "API Invalid: Missing Name" }
+
+        val type = rawJson["type"] as? String
+
+        return when (type) {
+            "ELECTRONIC" -> {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Electronic(id, name, warranty)
+            }
+            "CLOTHING" -> {
+                val size = rawJson["size"] as? String ?: "All Size"
+                Clothing(id, name, size)
+            }
+            else -> null
+        }
+    }
+}
