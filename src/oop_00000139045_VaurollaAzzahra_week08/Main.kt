e63e4475c64d189122d1414960cdf856a0e90266
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 16:19:33 +0700

    week08: simulate unit test failure using !!

--- src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
@@ -66,4 +66,6 @@ fun main () {
     // Kita menekan tombol !! karena KITA TAHU implementasi Java-nya aman
     val statusLength = javaResponse!!.length
     println("Status dari Java: $javaResponse (Length: $statusLength)")
+
+    runMockUnitTest()
 }
