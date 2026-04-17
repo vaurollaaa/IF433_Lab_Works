e37f3fe432623dd0337908e55941a56b81dfdeee
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 15:40:37 +0700

    week08: implement chained safe calls and elvis

---
+++ src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000139045_VaurollaAzzahra_week08
+
+fun Main () {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    // Rantai Safe Calls yang elegan
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+}
