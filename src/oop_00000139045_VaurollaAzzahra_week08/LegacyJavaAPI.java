4f32d8c3826feca34bbb3e1469f8df60c1dc9d1b
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 16:15:02 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000139045_VaurollaAzzahra_week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000139045_VaurollaAzzahra_week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_0K";
+    }
+}
