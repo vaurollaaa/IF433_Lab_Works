5c1c350e33b5e506ce94844a530e61f2eec6e2d8
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 15:35:21 +0700

    week08: create nested classes for Order

---
+++ src/oop_00000139045_VaurollaAzzahra_week08/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_00000139045_VaurollaAzzahra_week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
