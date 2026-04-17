b514962a8c788f35461e7f86d318d44b3f4783a5
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 21:12:18 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000139045_VaurollaAzzahra_week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000139045_VaurollaAzzahra_week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
