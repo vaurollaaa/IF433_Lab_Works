d776254c66e27b064e41a646aef1a799d461b27b
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 15:44:03 +0700

    week08: create mixed data collection

--- src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
@@ -18,4 +18,13 @@ fun Main () {
     } ?: "Transaksi Invalid: Harga belum di-set!"
 
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }
