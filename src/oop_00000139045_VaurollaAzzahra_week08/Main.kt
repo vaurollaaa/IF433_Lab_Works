5f6ff9a27a2b5c2b5aa5b0f6a0180c05af32428b
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-17 16:05:32 +0700

    week08: create fallback logic with as? and elvis

--- src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week08/Main.kt
@@ -36,4 +36,9 @@ fun main () {
             println("Ditemukan teks: ${it.uppercase()}")
         }
     }
+
+    val someObject: Any = 100 // Tipe aslinya Integer
+    // Coba cast ke String. Jika gagal (null), ganti dengan "Unknown String"
+    val safeString = someObject as? String ?: "Unknown String"
+    println("Hasil cast fallback: $safeString")
 }
