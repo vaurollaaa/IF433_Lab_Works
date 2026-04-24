71681112469f46ed3dcd81057b3c9ae2f56d55ee
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-24 14:05:41 +0700

    week09: (task) extract losing trades using filter

--- src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
+++ src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
@@ -12,4 +12,6 @@ fun main() {
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
 
     val winningTrades = closedTrades.filter { it.roe > 0 }
+
+    val losingTrades = closedTrades.filter { it.roe <= 0 }
 }
