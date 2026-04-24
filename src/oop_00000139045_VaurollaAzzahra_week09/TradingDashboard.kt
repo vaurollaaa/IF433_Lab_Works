623e1427964c243fa953689711fce521f7d47d99
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-24 14:01:37 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_00000139045_VaurollaAzzahra_week09
+
+fun main() {
+    // 2. Inisialisasi Data Uji
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 50, 45.0, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 5, 8.5, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 15, -12.0, "CLOSED"),
+        TradeLog("DOGEUSDT", "SHORT", 10, 2.5, "OPEN")
+    )
+
+}
