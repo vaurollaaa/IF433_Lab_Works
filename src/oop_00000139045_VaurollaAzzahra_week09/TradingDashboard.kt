4bb961c1f6c3743e0c07c4060f82fe642c928843
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-24 14:22:14 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
+++ src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
@@ -26,4 +26,7 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
 }
