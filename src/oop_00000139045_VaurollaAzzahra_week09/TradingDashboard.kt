f8dd9f905af2889c5ff6410d2556419ec6202d30
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-24 14:04:50 +0700

    week09: (task) extract winning trades using filter

--- src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
+++ src/oop_00000139045_VaurollaAzzahra_week09/TradingDashboard.kt
@@ -10,4 +10,6 @@ fun main() {
         TradeLog("DOGEUSDT", "SHORT", 10, 2.5, "OPEN")
     )
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+
+    val winningTrades = closedTrades.filter { it.roe > 0 }
 }
