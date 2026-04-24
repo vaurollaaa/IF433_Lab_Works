b9d72b1a66e566a07a1637bff23402e977b6644c
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-24 13:59:26 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000139045_VaurollaAzzahra_week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000139045_VaurollaAzzahra_week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
