f9232ac039d69fbc046b195e9d647cb7cb75ba3c
vaurollaaa <vaurollaazhr@gmail.com>
2026-04-24 12:30:26 +0700

    week09: create immutable and mutable list

---
+++ src/oop_00000139045_VaurollaAzzahra_week09/CollectionsBasics.kt
@@ -0,0 +1,15 @@
+package oop_00000139045_VaurollaAzzahra_week09
+
+fun main () {
+    println("=== TEST LIST ===")
+    // Immutable List: Tidak bisa diubah setelah dibuat
+    val frameworks: List<String> = listOf("Kotlin", "Java", "C++")
+    // frameworks.add("Python") // UNCOMMENT INI AKAN ERROR!
+    println("Immutable List: $frameworks")
+
+    // Mutable List: Bisa ditambah/dikurangi
+    val scores: MutableList<Int> = mutableListOf(85, 98)
+    scores.add(95)
+    scores[0] = 88
+    println("Mutable List: $scores")
+}
