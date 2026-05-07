95f8d1bad542cacd7614085c7dde708817b76383
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-07 21:49:54 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000139045_VaurollaAzzahra_week10/CryptoDashboard.kt
+++ src/oop_00000139045_VaurollaAzzahra_week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 2.0))
     coinRepo.add(Coin("USDT", 100.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
