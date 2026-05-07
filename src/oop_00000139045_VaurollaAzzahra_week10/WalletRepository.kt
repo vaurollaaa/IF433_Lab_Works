e7d03db8aa58ce3c9d214a4e8426d8a31a706716
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-07 21:33:34 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000139045_VaurollaAzzahra_week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_00000139045_VaurollaAzzahra_week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
