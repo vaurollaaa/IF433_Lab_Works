51bcda38c0d070a9d24d7a51ba2cde3cf0bdc982
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-07 21:45:18 +0700

    week10: (task) populate coin repository

--- src/oop_00000139045_VaurollaAzzahra_week10/CryptoDashboard.kt
+++ src/oop_00000139045_VaurollaAzzahra_week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000139045_VaurollaAzzahra_week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 100.0))
 }
