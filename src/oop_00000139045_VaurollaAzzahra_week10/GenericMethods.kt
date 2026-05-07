fc3456a600b8e8f9be7ef294b7d8cc2709f089e1
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-07 21:22:33 +0700

    week10: test generic functions with Double and String

--- src/oop_00000139045_VaurollaAzzahra_week10/GenericMethods.kt
+++ src/oop_00000139045_VaurollaAzzahra_week10/GenericMethods.kt
@@ -2,8 +2,8 @@ package oop_00000139045_VaurollaAzzahra_week10
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+}
 
     fun <T> processData(input: T): T {
         return input
     }
-}
--- src/oop_00000139045_VaurollaAzzahra_week10/Main.kt
+++ src/oop_00000139045_VaurollaAzzahra_week10/Main.kt
@@ -11,4 +11,9 @@ fun main() {
     println("\n=== TEST MULTIPLE PARAMETERS ===")
     val itemPrice = PairBox("Bitcoin", 65000)
     println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
+
+    println("\n=== TEST GENERIC FUNCTIONS ===")
+    printData(3.14)
+    val result = processData("Stable Coin")
+    println("Hasil proses: $result")
 }
