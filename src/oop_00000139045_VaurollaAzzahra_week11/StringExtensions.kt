ca99eaa3a1cbe0258d2e5c6318006b1728e01ae1
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-08 08:59:50 +0700

    week11: create nullable receiver extension

--- src/oop_00000139045_VaurollaAzzahra_week11/StringExtensions.kt
+++ src/oop_00000139045_VaurollaAzzahra_week11/StringExtensions.kt
@@ -9,3 +9,8 @@ fun String.addGreeting(): String {
 fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
 }
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    // 'this' bisa bernilai null, jadi harus ditangani
+    return this == null || this.isEmpty()
+}
