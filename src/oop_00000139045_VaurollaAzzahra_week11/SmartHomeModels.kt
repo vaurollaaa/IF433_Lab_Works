9d5f8e9ace747a82e8202cd2818268a6ac910cb9
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-08 09:22:25 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000139045_VaurollaAzzahra_week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000139045_VaurollaAzzahra_week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
