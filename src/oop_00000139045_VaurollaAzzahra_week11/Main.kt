bd8596465e46dd8117f6fc834dea8a5944ca879c
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-08 09:01:48 +0700

    week11: test extension functions in main

---
+++ src/oop_00000139045_VaurollaAzzahra_week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000139045_VaurollaAzzahra_week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
