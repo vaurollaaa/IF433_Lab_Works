9eeea61927bd3fccf9f8e711cb1deee9ad0cffea
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-15 16:55:20 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000139045_VaurollaAzzahra_week12/SmartFeeder.kt
@@ -0,0 +1,6 @@
+package oop_00000139045_VaurollaAzzahra_week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    return 0
+}
