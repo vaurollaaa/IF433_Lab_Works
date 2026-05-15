984502ef21fa5da0f72281c9509eecb2a0b91508
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-15 17:28:05 +0700

    week12: (task) handle feeding failure using onFailure

--- src/oop_00000139045_VaurollaAzzahra_week12/SmartFeeder.kt
+++ src/oop_00000139045_VaurollaAzzahra_week12/SmartFeeder.kt
@@ -47,4 +47,9 @@ fun main() {
             currentKibbleStock = newStock
             println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
         }
+
+        .onFailure { error ->
+            println("Peringatan ke Pemilik: ${error.message}")
+            println("(Opsional: Berikan chicken jerky secara manual)")
+        }
 }
