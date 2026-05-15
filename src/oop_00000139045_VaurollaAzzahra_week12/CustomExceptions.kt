38930e8f61fa5513075c4274c251dc43735ffa48
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-15 16:25:36 +0700

    week12: create InsufficientFunds exception

---
+++ src/oop_00000139045_VaurollaAzzahra_week12/CustomExceptions.kt
@@ -0,0 +1,7 @@
+package oop_00000139045_VaurollaAzzahra_week12
+
+// Custom exception dengan membawa data tambahan
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
