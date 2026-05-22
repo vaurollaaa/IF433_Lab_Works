dfedbc77333862cc910a8ca94fc7677cd89f8568
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-22 22:56:42 +0700

    week13: implement appendText operation

--- src/oop_00000139045_VaurollaAzzahra_week13/BasicFileIO.kt
+++ src/oop_00000139045_VaurollaAzzahra_week13/BasicFileIO.kt
@@ -6,4 +6,7 @@ fun main() {
     val file = File("notes.txt")
     file.writeText("Line 1: Inisialisasi sistem.\n")
     println("File berhasil dibuat dan ditulis.")
+
+    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
+    println("Teks berhasil di-append.")
 }
