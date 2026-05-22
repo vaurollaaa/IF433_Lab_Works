7f0b908719d808711792a887acc9a00abfb7ce59
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-22 23:25:08 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000139045_VaurollaAzzahra_week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_00000139045_VaurollaAzzahra_week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
