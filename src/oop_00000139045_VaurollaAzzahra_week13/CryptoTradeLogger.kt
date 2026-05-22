a68cfaf441f42f43ef188a54ef33bb98348500b8
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-22 23:45:39 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_00000139045_VaurollaAzzahra_week13/CryptoTradeLogger.kt
+++ src/oop_00000139045_VaurollaAzzahra_week13/CryptoTradeLogger.kt
@@ -54,6 +54,9 @@ fun main() {
     saveTrades(simulatedTrades, "crypto_trades.csv")
 
     File("crypto_trades.csv").appendText("CORRUPT_ID, DOGEUSDT, Hold, XX,YY\n")
+
+    val loadedData = loadTrades("crypto_trades.csv")
+    val totalPnl = loadedData.sumOf { it.pnl }
 }
 
 
