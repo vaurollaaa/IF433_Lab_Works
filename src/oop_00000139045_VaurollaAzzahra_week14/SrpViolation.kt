367814a16ab4ff221a60801c2caaf294370eea77
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-29 22:54:42 +0700

    week13: implement basic writeText function

--- src/oop_00000139045_VaurollaAzzahra_week14/SrpViolation.kt
+++ src/oop_00000139045_VaurollaAzzahra_week14/SrpViolation.kt
@@ -1,4 +1,18 @@
 package oop_00000139045_VaurollaAzzahra_week14
 
-class SrpViolation {
+data class User(val name: String, val email: String, val age: Int)
+
+class UserManager {
+    // Business logic
+    fun validateUser(user: User): Boolean {
+        return user.email.contains("@") && user.age >= 18
+    }
+    // Database access
+    fun saveUserToDatabase (user: User) {
+        println("INSERT INTO users VALUES ('${user.name}', '${user.email}')")
+    }
+    // Email notification
+    fun sendWelcomeEmail (user: User) {
+        println("Sending email to: ${user.email}")
+    }
 }
