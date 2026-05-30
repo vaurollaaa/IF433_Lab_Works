80252388bf47405dba06826b10bed494d09fbd03
vaurollaaa <vaurollaazhr@gmail.com>
2026-05-30 07:22:43 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000139045_VaurollaAzzahra_week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_00000139045_VaurollaAzzahra_week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount: DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+class MemberDiscount: DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
